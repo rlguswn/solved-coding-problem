2c3a72e9a0592bc90edfd45c5abfa3cd305b0baf
rlguswn <0318joo@naver.com>
2023-05-03 16:17:08 +0900

    23.05.03

--- 1789_수들의 합.py
+++
@@ -1,8 +0,0 @@
-s = int(input())
-i, n = 0, 0
-
-while(n + i < s):
-    i += 1
-    n = n + i
-
-print(i)
---
+++ 1934_최소공배수.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for i in range(t):
+    a, b = map(int,input().split())
+    A, B = a, b
+
+    while a != 0:
+        b = b % a
+        a, b = b, a   
+
+    gcd = b
+    lcm = A * B // b
+    print(lcm)
